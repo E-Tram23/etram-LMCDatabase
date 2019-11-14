d705fcd2bb42db3d21543f8669012571f63f0bce
Eric Tram <ewt6@pitt.edu>
2019-11-14 15:47:58 -0500

    Add files via upload
    Changed the data structure to match MRBS

--- Add.php
+++ Add.php
@@ -7,8 +7,9 @@
       <div class="wrapper-main">
         <section class = "section-default">
           <form action="includes/Add-inc.php" method = "post">
-            <input type="text" name ="Equipment_Name" placeholder = "Equipment name">
-            <input type="number" name ="Equipment_Barcode" placeholder = "Barcode">
+            <input type="text" name ="room_name" placeholder = "Equipment name">
+            <input type="text" name ="description" placeholder = "Description">
+            <input type="number" name ="id" placeholder = "Barcode">
             <input type="date" name ="Date_Entered" placeholder = "Date Entered">
             <button type = "submit" name = "add-submit">Add Equipment </button>
           </form>
